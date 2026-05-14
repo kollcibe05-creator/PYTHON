8f52b4b1fe736c7911434aca53f0cc2481ea7ac4
Collins Kibet <kollcibe05@gmail.com>
2026-05-14 15:43:05 +0300

    Additional python tips

---
+++ index.py
@@ -0,0 +1,7 @@
+list_ = ["My", "Focus", "Never", "Ceases", 2, "Amuse", "Me", 4, "Ever"]
+#  words = for x in list_: 
+#     if type(x) == int: 
+#         indx = list_.index(x)
+#         list_.remove(index)
+#     return list_
+
