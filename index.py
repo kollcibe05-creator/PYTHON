0b1e2a2e2076aeb544b086711d26128a408f6809
Collins Kibet <kollcibe05@gmail.com>
2026-05-15 22:35:19 +0300

    python evaluation

--- index.py
+++ index.py
@@ -1,28 +1,9 @@
-# import ipdb
 list_ = set(["My", "Focus", "Never", "Ceases", 2, "Amuse", "Me", 4, "Ever", 2])
-#  words = for x in list_: 
-#     if type(x) == int: 
-#         indx = list_.index(x)
-#         list_.remove(index)
-#     return list_
+def word_evaluator(word):
+    for x in list_: 
+        if type(x) == int: 
+            indx = list_.index(x)
+            list_.remove(index)
+    return list_
 
-dict_ = dict(name="Collo", age=20, kilos=56, State="Focused")
-# print(dict_)
-def tracing_value(x):
-    print("We are about to start")
-    while x < 5:
-        print(x, "Not yet 5")
-        # ipdb.set_trace()
-        x+=1
-    print("Done!")
-
-# print(tracing_value(0))
-
-outside_the_function = "Hey, I'm outside."
-print(outside_the_function)
-def change_inside_to_outside():
-        outside_the_function = "Change to inside"
-        return outside_the_function
-
-print(change_inside_to_outside())
-print(outside_the_function)
+print(word_evaluator(list_))
