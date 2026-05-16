b5a68ae40deedbabc3986ab20eea032ca871ed07
Collins Kibet <kollcibe05@gmail.com>
2026-05-16 21:41:19 +0300

    backslash essense

--- index.py
+++ index.py
@@ -1,9 +1,9 @@
 list_ = set(["My", "Focus", "Never", "Ceases", 2, "Amuse", "Me", 4, "Ever", 2])
-def word_evaluator(word):
-    for x in list_: 
-        if type(x) == int: 
-            indx = list_.index(x)
-            list_.remove(index)
-    return list_
+# def word_evaluator(word):
+#     for x in list_: 
+#         if type(x) == int: 
+#             indx = list_.index(x)
+#             list_.remove(index)
+#     return list_
 
-print(word_evaluator(list_))
+# print(word_evaluator(list_))
