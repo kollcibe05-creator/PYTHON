f95d9a6980f9f22d955403d1114fe7450aec0a28
Collins Kibet <kollcibe05@gmail.com>
2026-05-17 13:21:33 +0300

    testing differences

---
+++ index.js
@@ -0,0 +1,11 @@
+const dog = "Scooby"
+
+switch(dog){
+    case("Bosco"): console.log("It's Bosco")
+    break;
+    case("Scooby"): console.log("It's Scooby")
+    break;
+    default: console.log("No dog exists here.")   
+    break;
+}
+
