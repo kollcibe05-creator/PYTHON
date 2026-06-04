9cf5021c3d25360da46a7dc51835551a476909a5
Collins Kibet <kollcibe05@gmail.com>
2026-06-04 18:17:28 +0300

    created the artists data

--- execute.sql
+++ execute.sql
@@ -1,6 +1,12 @@
-CREATE TABLE DAMN (
-ID INTEGER PRIMARY KEY, 
-Track TEXT, 
-Length TEXT, 
-Views INTEGER
-);
+INSERT INTO artists (name, genre)  VALUES ("Rihanna", "RnB"); 
+INSERT INTO artists (name, genre)  VALUES ("Beyonce", "RnB"); 
+INSERT INTO artists (name, genre)  VALUES ("The Weeknd", "Pop"); 
+INSERT INTO artists (name, genre)  VALUES ("Bad Bunny", "Latina"); 
+INSERT INTO artists (name, genre)  VALUES ("Kendrick Lamar", "Rap"); 
+INSERT INTO artists (name, genre)  VALUES ("Eminem", "Rap"); 
+INSERT INTO artists (name, genre)  VALUES ("Omah Lay", "Afro Beats"); 
+INSERT INTO artists (name, genre)  VALUES ("Nas", "Rap"); 
+INSERT INTO artists (name, genre)  VALUES ("Sombr", "Indie"); 
+INSERT INTO artists (name, genre)  VALUES ("The Killers", "Band"); 
+INSERT INTO artists (name, genre)  VALUES ("AC/DC", "Alternative"); 
+INSERT INTO artists (name, genre)  VALUES ("Lana Del Rey", "Pop"); 
