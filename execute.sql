d9c31f43ef15ead76ef5042934c67bd167383d2f
Collins Kibet <kollcibe05@gmail.com>
2026-06-09 19:06:19 +0300

    more artists

--- execute.sql
+++ execute.sql
@@ -13,3 +13,4 @@
 -- INSERT INTO artists (genre) VALUES ("Indie")
 INSERT INTO artists (name, genre) VALUES ("Imani Imani", "RnB + Soul")
 INSERT INTO artists (name, genre) VALUES ("Michael Jackson", "Pop")
+INSERT INTO artists (name, genre) VALUES ("Kid Cudi", "Rap")
