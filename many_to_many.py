dc6b6456669d95f0fccd1771b7bf26043f71d007
Collins Kibet <kollcibe05@gmail.com>
2026-07-11 07:42:41 +0300

    asscociation proxy _ Hybrid approach

--- many_to_many.py
+++ many_to_many.py
@@ -1,6 +1,7 @@
 from sqlalchemy import (create_engine, desc, CheckConstraint, PrimaryKeyConstraint, UniqueConstraint, Index, DateTime, Integer, String, Column, func)
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.declarative import Table, declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.association_proxy import association_proxy
 
 
 Base = declarative_base()
@@ -61,6 +62,7 @@ class Review(Base):
     updated_at = Column(DateTime(), onupdate=func.now())
     user_id = Column(Integer(), ForeignKey("users.id"))
 
+# Use of Association Object Model
 class GameUser(Base):
     __tablename__ = "game_users"
 
@@ -75,3 +77,68 @@ class GameUser(Base):
         return f'GameUser(game_id={self.game_id}, ' + \
             f'user_id={self.user_id})'
 
+################################################################
+# Skipping the use of intermediaries entirely and using the Review to join the two entirely: Association Proxy (Hybrid Approach)
+class Game(Base):
+    __tablename__ = "games"
+
+    id = Column(Integer(), primary_key=True)
+    title = Column(String())
+    genre = Column(String())
+    platform = Column(String())
+    price = Column(Integer())
+    created_at = Column(DateTime(), server_default=func.now())
+    updated_at = Column(DateTime(), onupdate=func.now())
+
+
+    reviews = relationship("Review", back_populates="game")
+    users = association_proxy("reviews", "user", creator=lambda us: Review(user=us))
+
+    def __repr__(self):
+
+            return f'Game(id={self.id}, ' + \
+                f'title={self.title}, ' + \
+                f'platform={self.platform})'
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    created_at = Column(DateTime(), server_default=func.now())
+    updated_at = Column(DateTime(), onupdate=func.now())
+
+    reviews = relationship("Review", back_populates="user")
+
+    games = association_proxy("reviews", "game", creator=lambda gm: Review(game=gm))
+
+
+    def __repr__(self):
+
+            return f'User(id={self.id}, ' + \
+                f'name={self.name})'
+
+
+class Review(Base):
+    __tablename__ = 'reviews'
+
+    id = Column(Integer(), primary_key=True)
+
+    score = Column(Integer())
+    comment = Column(String())
+    created_at = Column(DateTime(), server_default=func.now())
+    updated_at = Column(DateTime(), onupdate=func.now())
+
+    game_id = Column(Integer(), ForeignKey("games.id"))
+    user_id = Column(Integer(), ForeignKey("users.id"))
+
+    game = relationship('Game', back_populates="reviews")
+    user = relationship('User', back_populates="reviews")
+
+    
+    def __repr__(self):
+
+        return f'Review(id={self.id}, ' + \
+            f'score={self.score}, ' + \
+            f'game_id={self.game_id})'
