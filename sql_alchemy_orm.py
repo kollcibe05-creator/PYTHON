7ca60faecd6e9207a4e6f50c62b307e07ceb45f3
Collins Kibet <kollcibe05@gmail.com>
2026-07-22 10:31:38 +0300

    relationship must be imported

--- sql_alchemy_orm.py
+++ sql_alchemy_orm.py
@@ -3,7 +3,7 @@ from sqlalchemy import (create_engine, desc, CheckConstraint, PrimaryKeyConstrai
 
 from sqlalchemy.ext.declarative import declarative_base
 
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship 
 
 
 Base = declarative_base()
