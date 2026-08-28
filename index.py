4ef0a76ee958ed70590a62ce9c5beb958dc68210
Collins Kibet <kollcibe05@gmail.com>
2026-08-28 21:58:41 +0300

    re retro

--- index.py
+++ index.py
@@ -1,19 +1,3 @@
-#!/usr/bin/env python3
-from faker import Faker
+import re 
 
-fake = Faker()
-
-# def create_grade_report(student_grades):
-#     with open('grade_report.txt', 'w') as gr:
-#         for grade in student_grades:
-#                 gr.write(grade + "\n")
-# if __name__ == "__main__":
-#     student_grades = []
-#     grade = input("Student name, grade: ")
-#     while grade:
-#         student_grades.append(grade)
-#         grade = input("Student name, grade: ")
-        
-#     create_grade_report(student_grades)
-
-print(fake.job())
+re.compile(r'')
